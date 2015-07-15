5ddc6a7ef56519595ed0cc5c50ab548384a13f1f
Matt Campbell <mecampbell25@gmail.com>
2015-07-16 00:24:41 +0200

    Adding specs

---
+++ spec/import_export/adapter_spec.rb
@@ -0,0 +1,11 @@
+require_relative '../spec_helper'
+
+module ImportExport
+  describe Adapter do
+    subject { Adapter.new }
+
+    describe "#modifiers" do
+      pending
+    end
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,3 @@
+require 'import_export'
+require 'minitest/autorun'
+require 'minitest/pride'
